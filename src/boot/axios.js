11e76e3ef7d7b80b4f41ac732b26f7cffe3fcf07
chan-nni <rkdcksal02@gmail.com>
2024-06-21 15:38:42 +0900

    baseURL 변경

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -4,7 +4,7 @@ import { useTokenStore } from "src/stores/token-store";
 
 // Create an axios instance
 const api = axios.create({
-  baseURL: "https://15.165.109.152:8080",
+  baseURL: "http://15.165.109.152:8080",
   headers: {
     "Content-Type": "application/json",
   },
