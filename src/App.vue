15602586a9ab082de5c0263650de9a90882948c1
chan-nni <rkdcksal02@gmail.com>
2024-06-27 04:48:26 +0900

    토큰 갱신 테스트

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,13 @@
 </template>
 
 <script setup>
+import { onMounted } from "vue";
 import { useTokenStore } from "src/stores/token-store";
+
 defineOptions({
   name: "App",
 });
+
 onMounted(() => {
   const store = useTokenStore();
   store.loadToken();
