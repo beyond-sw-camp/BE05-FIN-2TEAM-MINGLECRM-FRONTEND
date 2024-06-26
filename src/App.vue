5e33730b0eab67cab46972b7e6b08c1cdccf9dff
chan-nni <rkdcksal02@gmail.com>
2024-06-27 04:57:28 +0900

    복원

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,7 @@
 </template>
 
 <script setup>
-import { onMounted } from "vue";
-import { useTokenStore } from "src/stores/token-store";
-
 defineOptions({
   name: "App",
-});
-
-onMounted(() => {
-  const store = useTokenStore();
-  store.loadToken();
 });
 </script>
